35976204f0a6fea68e01208d41d001bab16b48a2
ForeverWeLearn <theanh11052006@gmail.com>
2026-07-17 14:27:36 +0700

    feat: add supabase

--- src/db/__init__.py
+++
@@ -1,2 +0,0 @@
-from .main import DB as DB
-from .utils import table_to_dict_by_id as table_to_dict_by_id
--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
 import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, JSONResponse
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from supabase import Client, create_client
 
-from src.db import DB
 from src.utils import compress_chroma_db
+
+load_dotenv()
+
 
 origins = [
     "http://localhost:3000",
 ]
-
-
-class AttendanceReportItem(BaseModel):
-    id: str
-    fullname: str
-    present: bool
-
 
 app = FastAPI()
 app.add_middleware(
@@ -27,8 +23,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-db = DB()
-db.init()
+
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL", default=""),
+    os.environ.get("SUPABASE_KEY", default=""),
+)
 
 
 @app.get("/")
@@ -45,27 +44,3 @@ def get_chroma_db_archive():
         media_type="application/x-xz",
         filename=os.path.basename(file_path),
     )
-
-
-@app.get("/attendance")
-async def get_attendance_report():
-    rows = db.get_attendance_report()
-    return JSONResponse(rows)
-
-
-@app.get("/attendance/{id}")
-async def get_attendance(id: str):
-    id = id.upper()
-    row = db.get_attendance(id)
-    return JSONResponse(row)
-
-
-@app.post("/attendance/{id}")
-def set_attendance(id: str, present: bool):
-    id = id.upper()
-    db.set_attendance(id, present)
-
-
-@app.delete("/attendance")
-def delete_attendance():
-    db.delete_attendance()
